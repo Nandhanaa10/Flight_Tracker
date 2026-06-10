f416fbf9ad60c48bbd62b7a26361f89083a420af
Nandhanaa10 <nandhanaakantha@gmail.com>
2026-06-10 21:07:32 +0530

    Basic Version

---
+++ src/FlightTracker.js
@@ -0,0 +1,114 @@
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import React, { useEffect, useState } from "react";
+import { Rectangle, Polyline } from "react-leaflet";
+import L from 'leaflet'
+
+const airplanIcon = (heading)=>L.divIcon({
+  html: `<div style="transform:rotate(${heading}deg); font-size:20px; color:gold;">✈</div>`,
+  className: '',
+  iconSize: [20, 20],
+  iconAnchor: [10, 10]
+})
+const markerIcon = L.divIcon({
+  html: `<div style="font-size:20px;">📍</div>`,
+  className: '',
+  iconSize: [20, 20],
+  iconAnchor: [10, 10]
+})
+function LocationMarker({ position, setPosition }) {
+
+  const map = useMapEvents({
+    click(e) {
+      setPosition(e.latlng)
+    },
+
+  })
+
+
+  return position === null ? null : (
+    <Marker position={position} icon={markerIcon}>
+      <Popup>Latitude : {position.lat.toFixed(5)} , Longitude : {position.lng.toFixed(5)}</Popup>
+    </Marker>
+
+  )
+}
+function FlightData({ flights }) {
+
+  return (<div>
+    <h2>Flights</h2>
+    <ul>
+      {flights.map((flight, index) => (
+        <li key={index}>
+          ICAO24: {flight.icao24} | Callsign: {flight.callsign} | Lat: {flight.lat} | Lon: {flight.lng}
+        </li>
+      ))}
+    </ul>
+  </div>)
+}
+function FlightMarkers({ flights }) {
+  flights = flights.filter(f => f.lng !== null && f.lat !== null)
+  if (!flights || flights.length === 0) return null
+  return (<>
+    {flights.map((flight) => (<React.Fragment key={flight.icao24}>
+      <Marker position={[flight.lat, flight.lng]} icon={airplanIcon(flight.heading)} >
+        <Popup>
+          Callsign: {flight.callsign}<br />
+          From: {flight.route?.src_airport}, {flight.route?.src_country}<br />
+          To: {flight.route?.dst_airport}, {flight.route?.dst_country}<br />
+          Altitude: {flight.altitude}m | Speed: {flight.velocity}m/s
+        </Popup>
+      </Marker>
+      {flight.route && <Polyline
+        positions={[
+          [Number(flight.route?.src_lat), Number(flight.route?.src_lng)],
+          [flight?.lat, flight?.lng]
+        ]}
+        pathOptions={{ color: 'gold', weight: 2 }}
+      />}
+    </React.Fragment>
+    ))}
+
+  </>)
+}
+export default function FlightTracker() {
+  const [position, setPosition] = useState(null)
+  const [flights, setFlight] = useState([])
+  const rectangle = position !== null ? [
+    [position.lat - 1, position.lng - 1],
+    [position.lat + 1, position.lng + 1]
+  ] : null
+  const purple = { color: 'purple' }
+  useEffect(() => {
+    if (!position) return
+    const fetchFlights = () => {
+      fetch(`http://localhost:8000/flights?lat=${position.lat}&lng=${position.lng}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setFlight(data || {})
+      })
+      .catch((error) => console.error("Error:", error))
+    }
+    fetchFlights() // immediate first fetch
+    const interval = setInterval(fetchFlights, 15000)
+
+    return () => clearInterval(interval)
+  }, [position])
+
+  return (
+    
+      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%", cursor: "pointer" }}>
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+        />
+        <LocationMarker setPosition={setPosition} position={position} />
+        
+        <FlightMarkers flights={flights} />
+
+      </MapContainer>
+      
+    
+  );
+}
---
+++ src/fastapi.py
@@ -0,0 +1,118 @@
+from fastapi import FastAPI
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+import csv
+#why fastapi - met with cors error so to debug it had to
+airlines={}
+airports={}
+routes={}
+def load_airports():
+    with open ('airports.dat',newline="",encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        for x in reader:
+            if(x[4]=="\\N"):
+                continue
+            airports[x[4]]={"Name":x[1],"City":x[2],"Country":x[3],"IATA":x[4],"ICAO":x[5],"Lat":x[6],"Lng":x[7]}
+
+def load_routes():
+    global routes
+    with open('routes.dat', newline="",encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        for x in reader:
+            if x[0] == "\\N" or x[2] == "\\N" or x[4] == "\\N":
+                continue
+            routes.setdefault(x[0], []).append({"src": x[2], "dst": x[4]})
+def load_airlines():
+    global airlines
+    with open('airlines.dat', newline="",encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        for x in reader:
+            if x[3] == "\\N" or x[4] == "\\N":
+                continue
+            airlines[x[4]] = x[3] 
+        
+
+def get_route(callsign):
+    ICAOcode = callsign[:3]
+    IATAcode = airlines.get(ICAOcode)
+    if not IATAcode:
+        return None
+    route = routes.get(IATAcode)
+    if not route:
+        return None
+    src = airports.get(route[0]["src"])
+    dest = airports.get(route[0]["dst"])
+    if not src or not dest:
+        return None
+    return {
+        "src_airport": src.get("Name"),
+        "src_country": src.get("Country"),
+        "src_lat": src.get("Lat"),
+        "src_lng": src.get("Lng"),
+        "dst_airport": dest.get("Name"),
+        "dst_country": dest.get("Country"),
+    }
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    load_airlines()
+    load_airports()
+    load_routes()
+    yield  
+
+app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+@app.get("/flights")
+async def flights(lat:float,lng:float):
+    headers = {
+        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 Edg/148.0.0.0"
+    }
+    rectangle = [
+    [lat - 1, lng - 1],
+    [lat + 1, lng + 1]]
+    resp = requests.get(f"https://opensky-network.org/api/states/all?lamin={lat-1}&lomin={lng-1}&lamax={lat+1}&lomax={lng+1}")
+    resp.raise_for_status()
+    data = resp.json()
+    states = data.get("states") or []
+    states = [s for s in states if s[5] is not None and s[6] is not None]
+    states = sorted(states, key=lambda s: (s[6]-lat)**2 + (s[5]-lng)**2)
+    states = states[:20]
+    result = []
+    for flight in states:
+        callsign = flight[1].strip() if flight[1] else None
+        route = get_route(callsign) if callsign else None
+        result.append({
+        "icao24": flight[0],
+        "callsign": callsign,
+        "lat": flight[6],
+        "lng": flight[5],
+        "altitude": flight[7],
+        "velocity": flight[9],
+        "heading": flight[10],
+        "route": route
+        })
+    result = [f for f in result if f["route"] is not None]
+    result = [f for f in result if f["route"]["src_country"] != f["route"]["dst_country"]]
+    return result
+
